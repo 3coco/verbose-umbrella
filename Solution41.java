4abe5eaadc88a5a06bd9cb0f6eb389809c6af3ed
3coco <cocostrong1@outlook.com>
2022-02-27 00:40:33 -0800

    41. First Missing Positive

---
+++ Solution41.java
@@ -0,0 +1,25 @@
+class Solution41 {
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (nums[i - 1] != i) {
+                return i;
+            }
+        }
+        return n + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
